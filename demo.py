814bc2fb481f41e615242a3e8d0d3f0606ac1215
Thomas Gardner <tpgardner19@gmail.com>
2017-11-24 17:19:44 -0700

    added demo scaffolding

--- demo.py
+++ demo.py
@@ -1,6 +1,17 @@
 ''' Basic practice with the Shodan API '''
+import json
+
 import shodan
 
 
+def _read_config(path="config.json"):
+    ''' Read API info out of config file '''
+    with open(path, "r") as ifile:
+        return json.load(ifile)
+
+
 if __name__ == "__main__":
-    pass
+    config = _read_config()
+    api = shodan.Shodan(config["key"])
+    print(api)
+
