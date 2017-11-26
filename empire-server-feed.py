8c47fea978fa7c3e9e1606bd5dbe8f4068401762
Thomas Gardner <tpgardner19@gmail.com>
2017-11-25 22:31:55 -0700

    started parsing results

--- empire-server-feed.py
+++ empire-server-feed.py
@@ -1,6 +1,6 @@
 '''
-Query Shodan for open Empire HTTP listeners based on the following article:
-https://www.tenable.com/blog/identifying-empire-http-listeners
+Generate a CSV feed from Shodan data on open Empire HTTP listeners
+Based on the following article: https://www.tenable.com/blog/identifying-empire-http-listeners
 '''
 import csv
 import ipaddress
@@ -24,9 +24,24 @@ def search(api):
 
     :api: Shodan search API
     '''
-    query_terms = 'title:"404 Not Found" + "Content-Length: 233"  + "Cache-Control: no-cache, no-store, must-revalidate" -"post-check=" -"pre-check=" -"private" + "Pragma: no-cache" + "Expires: 0" + "Server:" -"X-" -"Set-Cookie:" -"Connection:" -"Etag" -"Last-Modified" -"Accept-Ranges:" -"Access-Control"'
-
+    query = 'title:"404 Not Found" + "Content-Length: 233"  + "Cache-Control: no-cache, no-store, must-revalidate" -"post-check=" -"pre-check=" -"private" + "Pragma: no-cache" + "Expires: 0" + "Server:" -"X-" -"Set-Cookie:" -"Connection:" -"Etag" -"Last-Modified" -"Accept-Ranges:" -"Access-Control"'
+    results = api.search(query)
+    print(results["total"])
+    for result in results["matches"]:
+        ip = ipaddress.ip_address(result["ip"])
+        port = result["port"]
+        city = result["location"]["city"]
+        country = result["location"]["country_code"]
+        ssl_fingerprint = result["ssl"]["cert"]["fingerprint"]["sha256"]
+        ssl_issued = result["ssl"]["cert"]["issued"]
+        ssl_expires = result["ssl"]["cert"]["expires"]
+        raw_link = "https://www.shodan.io/host/{}".format(ip)
+    print(json.dumps(results["matches"][0], indent=2))
 
 if __name__ == "__main__":
     CONFIG = read_config()
+    CSV_FILENAME = "empire-http-listeners.csv"
+    CSV_FIELDS = [ "ipaddress", "port", "country", "added-on" ]
+    FACETS = [ "org", "domain", "port", "asn", "country" ]
     api = shodan.Shodan(CONFIG["key"])
+    search(api)
